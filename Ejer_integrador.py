7fb177794088a9c1d116ac254956b21ec193f248
Felipe Reynoso <lipereynoso@gmail.com>
2025-04-30 00:51:06 -0300

    aplicamos try y except para verificar que los datos que se piden al usuario sean los correctos

--- Ejer_integrador.py
+++ Ejer_integrador.py
@@ -1,6 +1,14 @@
 import time
+# Importamos time para despues poder hacer uso de time.sleep
 # Solicitamos el número al usuario para que podamos hacer el conteo
-numero_maximo = int(input("Introduce el número decimal para iniciar el conteo: "))
+
+# Aplicamos "Try" y "Except" que fueron sugeridos por la IA nos recomendaba para poder verificar que los datos ingresados por el usuario sean los correctos
+while True:
+    try:
+        numero_maximo = int(input("Introduce el número decimal para iniciar el conteo: "))
+        break
+    except ValueError:
+        print("El valor no es correcto, intente nuevamente: ")
 print("Calculando...")
 time.sleep(3)
 
@@ -13,5 +21,5 @@ for i in range(numero_maximo + 1):
     # Eliminamos el prefijo '0b' mediante el operador [2:]
     print(f"{i} -> {binario}")
     # Aplicamos la funcion time.sleep para poder dar un tiempo para poder visualizar el conteo
-    time.sleep (3)
+    time.sleep (2)
 
